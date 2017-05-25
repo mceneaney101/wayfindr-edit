e9efe3f965eb2f27675a6fdde200591868310405
Paul McEneaney <paulmceneaney@Pauls-iMac.local>
2017-05-25 15:01:15 +0100

    Create a blank slate for practising MVC design pattern for Xcode

--- personal-wayfindr/ViewController.h
+++ personal-wayfindr/ViewController.h
@@ -10,10 +10,5 @@
 
 @interface ViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UILabel *helloInteraction;
-@property (weak, nonatomic) IBOutlet UITextView *customTextfield;
-@property (weak, nonatomic) IBOutlet UIButton *switchButton;
-
-
 @end
 
--- personal-wayfindr/ViewController.m
+++ personal-wayfindr/ViewController.m
@@ -16,41 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    /*
-    // Creating the Hello label and assigning it the size of the frame it needs
-    UILabel *hellolabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 200, 50)];
-    //Set the text property of the Hello label
-    hellolabel.text = @"Hello World!";
-    //Add the Hello label object to the view
-    [self.view addSubview:hellolabel];
-    */
-    
-    //label interaction
-    self.helloInteraction.text = @"Button interaction";
-    //[self.view addSubview:_helloInteraction];
-    
-    //Button interaction
-    [self.switchButton setTitle:@"Switch Click" forState:UIControlStateNormal];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-
-// actions for when the button is clicked
-- (IBAction)handleSwitchButton:(id)sender {
-    // assigning text value to customtextfield
-    //self.customTextfield.text = @"Button clicked";
-    self.customTextfield.text = self.helloInteraction.text;
-}
-
-- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    [self.customTextfield resignFirstResponder];
 }
 
 @end
