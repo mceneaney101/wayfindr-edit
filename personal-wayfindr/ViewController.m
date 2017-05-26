0f9ae659f58a2e108de6d9b78c407ff066a284db
Paul McEneaney <paulmceneaney@Pauls-iMac.local>
2017-05-26 11:06:10 +0100

    Added the UILabel and UIImageBox from the Main.StoryBoard to the view controller and added a bool method to view the location object values

--- personal-wayfindr/ViewController.m
+++ personal-wayfindr/ViewController.m
@@ -7,6 +7,8 @@
 //  banaanas 
 
 #import "ViewController.h"
+#import "Location.h"
+#import "LocationDataController.h"
 
 @interface ViewController ()
 
@@ -18,7 +20,18 @@
     [super viewDidLoad];
 }
 
-
+//a bool method to check if the view generated
+- (void)viewDidAppear:(BOOL)animated
+{
+    //assigns the LocationDataController class a object to interact with
+    LocationDataController *model = [[LocationDataController alloc] init];
+    //use method in 'model' to assign values of hardcoded address to poi
+    Location *poi = [model getPointOfInterest];
+    
+    //updating the UI objects with poi property values
+    self.addressLabel.text = poi.address;
+    [self.photoImageView setImage:[UIImage imageNamed:poi.photoFileName]];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
